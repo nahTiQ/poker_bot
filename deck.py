c394675c370e6230bf778e36e8a06e910f777af5
nahTiQ <bryantosm@gmail.com>
2019-10-27 22:50:19 -0400

    Add files via upload

--- deck.py
+++ deck.py
@@ -41,7 +41,15 @@ class Deck:
 	def rebuild_deck(self):
 		''' Reset the deck back to it's original state
 		    Copy / Paste self.cards = [list] here'''
-		print('')
+		self.cards = ['Ace-Clubs', 'King-Clubs', 'Queen-Clubs', 'Jack-Clubs', '10-Clubs', '9-Clubs',
+					'8-Clubs', '7-Clubs', '6-Clubs', '5-Clubs', '4-Clubs', '3-Clubs', '2-Clubs',
+					'Ace-Spades', 'King-Spades', 'Queen-Spades', 'Jack-Spades', '10-Spades', 
+					'9-Spades', '8-Spades', '7-Spades', '6-Spades', '5-Spades', '4-Spades', '3-Spades',
+					'4-Spades', '3-Spades', '2-Spades', 'Ace-Hearts', 'King-Hearts', 'Queen-Hearts',
+					'Jack-Hearts', '10-Hearts', '9-Hearts', '8-Hearts', '7-Hearts', '6-Hearts', 
+					'5-Hearts', '4-Hearts', '3-Hearts', '2-Hearts', 'Ace-Diamonds', 'King-Diamonds',
+					'Queen-Diamonds', 'Jack-Diamonds', '10-Diamonds', '9-Diamonds', '8-Diamonds', 
+					'7-Diamonds', '6-Diamonds', '5-Diamonds', '4-Diamonds', '3-Diamonds', '2-Diamonds'  ]
 
 	def print_deck(self):
 		'''Print cards left in the deck. This is mainly for troubleshooting purposes.'''
--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from table import Table
 
 # idk where to put this, so it's going here
 # Creates player variables, tho
-def Create_Players(number_of_players):
+def create_players(number_of_players):
 	number_of_players = int(number_of_players)
 	for number in range(0, number_of_players):
 		list_of_player_names[number] = Player(list_of_player_names[number])
@@ -19,7 +19,7 @@ table = Table()
 number_of_players = input("How many players would you like to play with? (1-7) ")
 
 table.players_at_table(number_of_players)
-Create_Players(number_of_players)
+create_players(number_of_players)
 for item in list_of_player_names:
 	try:
 		print(f'{item.name.title()} has {item.hand} in his hand')
